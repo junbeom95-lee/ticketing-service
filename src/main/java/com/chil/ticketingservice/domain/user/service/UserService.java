5f3573fa5b5c9165829b74b79bdfeca099a5b4eb
i-am-dahee <hxxly02@gmail.com>
2026-01-02 00:00:40 +0900

    feat: 회원 권한 추가
    - UserRole 생성 (ADMIN, USER)
    - 회원가입 시 권한 선택 불가 → 기본값 USER

--- src/main/java/com/chil/ticketingservice/domain/user/service/UserService.java
+++ src/main/java/com/chil/ticketingservice/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.chil.ticketingservice.domain.user.service;
 import com.chil.ticketingservice.domain.user.dto.request.UserCreateRequest;
 import com.chil.ticketingservice.domain.user.dto.response.UserCreateResponse;
 import com.chil.ticketingservice.domain.user.entity.User;
+import com.chil.ticketingservice.domain.user.enums.UserRole;
 import com.chil.ticketingservice.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,7 +23,8 @@ public class UserService {
                 request.getEmail(),
                 request.getUsername(),
                 request.getBirth(),
-                request.getPassword()
+                request.getPassword(),
+                UserRole.USER
         );
 
         User savedUser = userRepository.save(user);
