408cfdaa5cbc2a330045be57aac2b3fe474da368
LEE SEOJUN <junbeom95@gmail.com>
2026-01-02 09:37:35 +0900

    feat: 좌석 Entity, Repository 생성

---
+++ src/main/java/com/chil/ticketingservice/domain/seat/entity/Seat.java
@@ -0,0 +1,32 @@
+package com.chil.ticketingservice.domain.seat.entity;
+
+import com.chil.ticketingservice.domain.show.entity.Show;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "seats")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "seat_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "show_id", nullable = false)
+    private Show show;
+
+    @Column(nullable = false)
+    private String seatType;
+
+    @Column(nullable = false)
+    private int seatNumber;
+
+    @Column(nullable = false)
+    private Boolean seatStatus;
+}
