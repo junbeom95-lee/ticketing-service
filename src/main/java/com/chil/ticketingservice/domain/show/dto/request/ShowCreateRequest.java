a3e3f8da997545aac1c9f6bc3c896a065cdf5fef
Kingyulim <youremail@example.com>
2026-01-02 16:51:52 +0900

    chore: 필요없는 주석 제거

--- src/main/java/com/chil/ticketingservice/domain/show/dto/request/ShowCreateRequest.java
+++ src/main/java/com/chil/ticketingservice/domain/show/dto/request/ShowCreateRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -16,10 +15,6 @@ public record ShowCreateRequest(
         String location,
 
         @NotNull(message = "공연 일정이 입력되지 않았습니다.")
-        /*@Pattern(
-                regexp = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]) ([01]\\d|2[0-3]):[0-5]\\d$",
-                message = "날짜 형식은 yyyy-MM-dd HH:mm 입니다."
-        )*/
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
         LocalDateTime showDate,
 
