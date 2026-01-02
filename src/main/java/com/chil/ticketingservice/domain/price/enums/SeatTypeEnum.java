18b664d032528cd3f518c3c948e40737b42387aa
Kingyulim <youremail@example.com>
2026-01-02 19:29:05 +0900

    remove: 상수 String 검증 인스턴트 메서드 삭제

--- src/main/java/com/chil/ticketingservice/domain/price/enums/SeatTypeEnum.java
+++ src/main/java/com/chil/ticketingservice/domain/price/enums/SeatTypeEnum.java
@@ -1,23 +1,9 @@
 package com.chil.ticketingservice.domain.price.enums;
 
-import com.chil.ticketingservice.common.enums.ExceptionCode;
-import com.chil.ticketingservice.common.exception.CustomException;
 import lombok.Getter;
-
-import java.util.Arrays;
 
 @Getter
 public enum SeatTypeEnum {
 
     VIP, S, A, B;
-
-    public static SeatTypeEnum of(String value){
-        return Arrays
-                .stream(SeatTypeEnum.values())
-                .filter(s -> s.name().equalsIgnoreCase(value))
-                .findFirst()
-                .orElseThrow(
-                        () -> new CustomException(ExceptionCode.SHOW_INVALID_SEAT_TYPE)
-                );
-    }
 }
