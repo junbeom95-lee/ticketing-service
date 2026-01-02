68c3d22166b8ae3531c4175cbde2ccc72755aae5
castlejuyeon <jucess1106@naver.com>
2026-01-02 11:24:10 +0900

    refactor: BookingService 클래스 레벨 @Transactional(readOnly) 제거

--- src/main/java/com/chil/ticketingservice/domain/booking/service/BookingService.java
+++ src/main/java/com/chil/ticketingservice/domain/booking/service/BookingService.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class BookingService {
 
     private final BookingRepository bookingRepository;
