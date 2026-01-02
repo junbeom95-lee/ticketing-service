6895e4649d3c958adc7b336ed54adcb91ea01848
Kingyulim <youremail@example.com>
2026-01-02 19:34:03 +0900

    refactor: 메세지마다 마침표 추가

--- src/main/java/com/chil/ticketingservice/common/enums/SuccessMessage.java
+++ src/main/java/com/chil/ticketingservice/common/enums/SuccessMessage.java
@@ -5,18 +5,18 @@ import lombok.Getter;
 @Getter
 public enum SuccessMessage {
 
-    AUTH_LOGIN_SUCCESS("로그인 성공"),
-    USER_CREATE_SUCCESS("회원가입 성공"),
-    USER_GET_SUCCESS("회원 조회 성공"),
+    AUTH_LOGIN_SUCCESS("로그인 성공."),
+    USER_CREATE_SUCCESS("회원가입 성공."),
+    USER_GET_SUCCESS("회원 조회 성공."),
 
     // 공연
-    SHOW_CREATE_SUCCESS("공연이 생성되었습니다"),
-    SHOW_RESPONSE_SUCCESS("공연 검색 성공"),
+    SHOW_CREATE_SUCCESS("공연이 생성되었습니다."),
+    SHOW_RESPONSE_SUCCESS("공연 검색 성공."),
     SHOW_CREATE_SEAT_SUCCESS("공연 좌석 가격이 생성 되었습니다."),
 
     // 예매
-    BOOKING_CREATE_SUCCESS("예매 성공"),
-    BOOKING_CANCEL_SUCCESS("예매 취소 성공"),
+    BOOKING_CREATE_SUCCESS("예매 성공."),
+    BOOKING_CANCEL_SUCCESS("예매 취소 성공."),
     BOOKING_GET_SUCCESS("예매 조회 성공"),
 
     ;
