f35a7eaaa9d21b2db869b4604da4c133476ff9ab
i-am-dahee <hxxly02@gmail.com>
2026-01-02 14:25:47 +0900

    refactor: 핸들러 수정
    - @RequiredArgsConstructor 추가
    - final 추가

--- src/main/java/com/chil/ticketingservice/common/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/chil/ticketingservice/common/jwt/JwtAccessDeniedHandler.java
@@ -5,6 +5,7 @@ import com.chil.ticketingservice.common.enums.ExceptionCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -12,9 +13,10 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
