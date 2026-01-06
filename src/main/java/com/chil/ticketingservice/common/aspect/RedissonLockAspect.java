98b19056bab667d00a6e66c1680338d1161ebd59
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-06 11:41:16 +0900

    feat: Redisson을 활용한 동시성 이슈 제어
    - build.gradle
    1. Redisson 의존성 추가
    
    - RedissonLockAspect
    1. RedissonClient.getLock(lockKey) : lockKey로 RLock 객체 생성
    2. RLock.tryLock(...) : 주어진 시간 동안 락 획득 시도, 락 획득 여부 반환
    3. RLock.isHeldByCurrentThread() : 현재 쓰레드가 해당 락을 보유하고 있는지 여부 반환
    4. RLock.unlock() : 현재 쓰레드가 보유한 락 해제

---
+++ src/main/java/com/chil/ticketingservice/common/aspect/RedissonLockAspect.java
@@ -0,0 +1,62 @@
+package com.chil.ticketingservice.common.aspect;
+
+import com.chil.ticketingservice.common.annotation.RedisLock;
+import com.chil.ticketingservice.domain.booking.dto.request.BookingCreateRequest;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Aspect
+@Component
+public class RedissonLockAspect {
+
+    private final RedissonClient redissonClient;
+
+    @Around("@annotation(redisLock)")
+    public Object around(ProceedingJoinPoint joinPoint, RedisLock redisLock) throws Throwable {
+
+        Object[] args = joinPoint.getArgs();
+        BookingCreateRequest requestDto = (BookingCreateRequest) args[1];
+
+        String lockKey = String.format("lock:showId:%d:seatId:%s", requestDto.showId(), requestDto.seat());
+        long waitTime = 0;
+        long leaseTime = 5;
+
+        RLock rLock = redissonClient.getLock(lockKey);
+
+        boolean lockAcquired = false;
+
+        try {
+
+            lockAcquired = rLock.tryLock(waitTime, leaseTime, TimeUnit.SECONDS);
+
+            if (!lockAcquired) {
+                log.warn("[RedissonLock] 락 획득 실패 - lockKey: {}", lockKey);
+                throw new IllegalStateException("락 획득 실패 - lockKey: " + lockKey);
+            }
+
+            log.info("[RedissonLock] 락 획득 성공 - lockKey: {}", lockKey);
+
+            return joinPoint.proceed();
+
+        } finally {
+
+            if (lockAcquired && rLock.isHeldByCurrentThread()) {
+                try {
+                    rLock.unlock();
+                    log.info("[RedissonLock] 락 해제 완료 - lockKey: {}", lockKey);
+                } catch (IllegalMonitorStateException e) {
+                    log.warn("[RedissonLock] 이미 해제된 락 또는 스레드 불일치 - lockKey: {}", lockKey, e);
+                }
+            }
+        }
+    }
+}
