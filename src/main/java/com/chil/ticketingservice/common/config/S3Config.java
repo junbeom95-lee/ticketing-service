6e5386f2db304b51b1f6f2c0851d53d70d15956f
i-am-dahee <hxxly02@gmail.com>
2026-01-07 10:57:33 +0900

    fix: application 파일 수정

--- src/main/java/com/chil/ticketingservice/common/config/S3Config.java
+++ src/main/java/com/chil/ticketingservice/common/config/S3Config.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!test")
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
