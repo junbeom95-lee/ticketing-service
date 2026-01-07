4d462178e4ab65cedef21017b981107e7cb2301a
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2026-01-07 13:02:29 +0900

    refactor: Redis에 시간 저장 시, currentTimeMillis 사용
    - 기존: LocalDateTime 사용 시, String -> LocalDateTime 변환에 문제가 생김
    - 변경 방향: LocalDateTime 대신 currentTimeMillis 사용하여 시간 저장

--- src/main/java/com/chil/ticketingservice/common/aspect/SchedulerEventLogAspect.java
+++ src/main/java/com/chil/ticketingservice/common/aspect/SchedulerEventLogAspect.java
@@ -1,11 +1,12 @@
 package com.chil.ticketingservice.common.aspect;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -14,11 +15,14 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SchedulerEventLogAspect {
 
-    private final RedisTemplate<String, Object> redisTemplate;
-    public static final String CACHE_PREFIX = "last_booking_time";
+    private final StringRedisTemplate redisTemplate;
+    public static final String LAST_BOOKING_TIME = "last_booking_time";
 
     @AfterReturning("execution(* com.chil.ticketingservice.domain.booking.service.BookingService.createBooking(..))")
-    public void setLastCreatedTime() {
-        redisTemplate.opsForValue().set(CACHE_PREFIX, LocalDateTime.now().toString());
+    public void setLastBookingTime() {
+        redisTemplate.opsForValue().set(
+            LAST_BOOKING_TIME,
+            String.valueOf(System.currentTimeMillis()),
+            15, TimeUnit.MINUTES);
     }
 }
